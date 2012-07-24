994676397fd1e66fc4c7cf118b85cbaae1f1aaff
Yury Yantsevich <featalion@gmail.com>
2012-07-25 02:47:35 +0300

    do not use built-in layout; overload current_page? Action View helper for engine

--- app/helpers/audited_actions/application_helper.rb
+++ app/helpers/audited_actions/application_helper.rb
@@ -1,4 +1,16 @@
 module AuditedActions
   module ApplicationHelper
+    def current_page?(opts)
+      super(opts) unless request && request.get?
+
+      controller = opts.delete(:controller).to_s
+      if controller == 'audited_actions'
+        url_str = url_for(opts)
+
+        request.path.end_with?(opts)
+      else
+        false
+      end
+    end
   end
 end
