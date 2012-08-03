a810eb032ab4492049ab81231d920862135d29c0
Yury Yantsevich <featalion@gmail.com>
2012-08-03 18:10:12 +0300

    better per_page parsing

--- app/controllers/audited_actions/audited_actions_controller.rb
+++ app/controllers/audited_actions/audited_actions_controller.rb
@@ -10,7 +10,7 @@ module AuditedActions
 
     def index
       @page = params[:page].to_i
-      @per_page = [params[:per_page].to_i, 1].max
+      @per_page = [(params[:per_page] || 10).to_i, 1].max
 
       all_entries = AuditedActionsLogEntry.all
       offset = ([@page, 1].max - 1) * @per_page
