14e2e952b30438e3458f35ba59aba592f516b634
Yury Yantsevich <featalion@gmail.com>
2012-08-07 05:14:12 +0300

    removed odd helper

--- app/helpers/audited_actions/application_helper.rb
+++ app/helpers/audited_actions/application_helper.rb
@@ -1,16 +1,4 @@
 module AuditedActions
   module ApplicationHelper
-    def current_page?(opts)
-      super(opts) unless request && request.get?
-
-      controller = opts.delete(:controller).to_s
-      if controller == 'audited_actions'
-        url_str = url_for(opts)
-
-        request.path.end_with?(opts)
-      else
-        false
-      end
-    end
   end
 end
