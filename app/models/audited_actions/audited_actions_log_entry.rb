1f952ff616a894ae3343d8989555e8bc08a34c63
Yury Yantsevich <featalion@gmail.com>
2012-08-08 17:14:27 +0300

    added simple caching for associated models also as possibility to reload them all

--- app/models/audited_actions/audited_actions_log_entry.rb
+++ app/models/audited_actions/audited_actions_log_entry.rb
@@ -19,21 +19,44 @@ module AuditedActions
 
     # belongs_to :user
     def actor
-      _actor['__klass'].constantize.find(_actor['__id']) rescue nil
+      @actor ||= _actor['__klass'].constantize.find(_actor['__id']) rescue nil
     end
 
-    def method_missing(meth, *args)
+    def method_missing(method, *args)
+      @associated_models ||= {}
+      meth_s = method.to_s
+      # associated model and was found?
+      return @associated_models[meth_s] if @associated_models[meth_s]
       # try to find method name in the associations
-      if aattr = _associations[meth.to_s]
-        if aattr.is_a?(Hash) && aattr['__klass'] && aattr['__id']
-          aattr['__klass'].constantize.find(aattr['__id']) rescue nil
+      if aattr = _associations[meth_s]
+        if model?(aattr)
+          @associated_models[meth_s] = find_associated_model(aattr)
         else
           aattr
         end
       else
+        # is not association
         super
       end
     end
+
+    def reload_associations
+      _associations.each do |name, value|
+        if model?(value)
+          @associated_models[name] = find_associated_model(value)
+        end
+      end
+    end
+
+    private
+    def model?(var)
+      var.is_a?(Hash) && var['__klass'] && var['__id']
+    end
+
+    def find_associated_model(opts)
+      opts['__klass'].constantize.find(opts['__id']) rescue nil
+    end
+
   end
 
 end
