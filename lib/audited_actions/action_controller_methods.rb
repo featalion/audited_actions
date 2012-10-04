0965c16a5df101a112f909ee0530d05ab9ef1a56
Yury Yantsevich <featalion@gmail.com>
2012-10-04 02:42:12 +0200

    changes data store for audited actions; updates working with conditions

--- lib/audited_actions/action_controller_methods.rb
+++ lib/audited_actions/action_controller_methods.rb
@@ -16,32 +16,27 @@ module AuditedActions
 
         include AuditedActions::ActionControllerMethods::LocalInstanceMethods
 
+        # get all actions because :only rewrites previous filters installation
+        actions = audited_actions_data.keys
         if opts[:before_action]
-          before_filter :log_audited_action, only: args
+          before_filter :log_audited_action, only: actions
         else
-          after_filter :log_audited_action, only: args
+          after_filter :log_audited_action, only: actions
         end
       end
 
       def audited_actions_store_actions_data(actions, options)
-        cattr_accessor :audited_actions_actor_for
-        self.audited_actions_actor_for ||= {}
-
-        cattr_accessor :audited_actions_associations_for
-        self.audited_actions_associations_for ||= {}
-
-        cattr_accessor :audited_actions_condition_for
-        self.audited_actions_condition_for ||= {}
+        cattr_accessor :audited_actions_data
+        self.audited_actions_data ||= {}
 
         actor = (options[:actor] || AuditedActions::Engine.config.current_user).to_sym
         associations = audited_actions_parse_associations(options[:associate])
         # Set all data per action,
         # it allows multiple calls of `audited_actions` method on controller
         actions.each do |action|
-          self.audited_actions_actor_for[action] = actor
-          self.audited_actions_associations_for[action] = associations
+          self.audited_actions_data[action] = {actor: actor, associations: associations}
           if options[:if] && options[:if].is_a?(Proc)
-            self.audited_actions_condition_for[action] = options[:if]
+            self.audited_actions_data[action][:condition] = options[:if]
           end
         end
       end
@@ -68,8 +63,8 @@ module AuditedActions
         action_sym = params['action'].to_sym
 
         # condition check
-        condition = audited_actions_condition_for[action_sym]
-        return if condition && !condition.call
+        condition = audited_actions_data[action_sym][:condition]
+        return if condition && !instance_exec(&condition)
 
         log_data = {
           # using `params` & `request` defined by Rails
@@ -78,12 +73,12 @@ module AuditedActions
           action_object_id: params['id'],
           fullpath: request.fullpath,
           # actor for current action
-          _actor: send(audited_actions_actor_for[action_sym])
+          _actor: send(audited_actions_data[action_sym][:actor])
         }
 
         associations = {}
         # each association for current action
-        audited_actions_associations_for[action_sym].each do |key, var_name|
+        audited_actions_data[action_sym][:associations].each do |key, var_name|
           associations[key] = self.instance_variable_get("@#{var_name}")
         end
         log_data[:_associations] = associations
